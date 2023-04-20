b872a113ce947db0d517a7aced971eb78a844f61
Sag is the best <87934102+msajo@users.noreply.github.com>
2023-04-20 16:15:20 +1000

    Add files via upload

--- Tvroom.js
+++ Tvroom.js
@@ -1,14 +1,28 @@
 Img = "";
+status2 = "";
 function preload(){
     Img = loadImage("Tv and Ac.jpeg");
 }
 function setup(){
     Canvas = createCanvas(640,420);
     Canvas.center();
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting objects";
 }
 function draw(){
     image(Img,0,0,640,420);
 }
 function back(){
-    Window.location("index.html");
+    window.location = "index.html";
+}
+function modelLoaded(){
+    console.log("ModelLoaded");
+    status2 = true;
+    objectDetector.detect(Img,gotposes);
+}
+function gotposes(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
 }
